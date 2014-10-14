93b603557ad3f5c71eea13d534d9366af9250331
alexfakhri <alexfakhri@me.com>
2014-10-14 11:56:41 +0100

    Knows when a number is divisible by 5 and 3

---
+++ spec/JavabuzzSpec.js
@@ -0,0 +1,38 @@
+describe('Javabuzz', function () {
+
+	beforeEach( function() {
+		javabuzz = new Javabuzz();
+	});
+
+	var javabuzz
+
+	describe('knows when a number is', function () {
+
+		it ('divisible by 3', function () {
+			expect(javabuzz.isDivisibleByThree(3)).toEqual(true)
+		});
+
+		it ('divisible by 5', function () {
+			expect(javabuzz.isDivisibleByFive(5)).toEqual(true)
+		});
+
+	});
+	
+	describe('knows when a number is NOT', function () {
+
+		it ('divisible by 3', function()  {
+			expect(javabuzz.isDivisibleByThree(1)).toEqual(false)
+		});
+	});
+
+
+
+
+
+	
+
+
+
+
+});
+
